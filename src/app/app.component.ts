6847a763d7d90ea14c3be8a8ac6c4f25b75dbdf3
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-26 12:29:50 +0530

    Create a custom percentage pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  price = 100;
+  discount = 5;
+
+  // updatePrice(price: string) {
+  //   this.price = +price;
+  // }
+  //
+  // updateDiscount(discount: string) {
+  //   this.discount = +discount;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { PercentagePipe } from './pipe/percentage.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PercentagePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   exports: [],
   providers: [],
