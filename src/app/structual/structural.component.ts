d596735e303fcb82a87212d23c525f0976b3af5a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-27 13:00:02 +0530

    ngFor

--- src/app/structual/structural.component.ts
+++ src/app/structual/structural.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuralComponent implements OnInit {
 
+  showParagraph = true;
+  students: Array<{id: string, name: string}> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.students.push({id: 'S001', name: 'Manoj'});
+    this.students.push({id: 'S003', name: 'Gayal'});
+    this.students.push({id: 'S005', name: 'Pethum'});
+    this.students.push({id: 'S007', name: 'Dinusha'});
   }
 
+  // toggleVisibility() {
+  //   this.showParagraph = !this.showParagraph;
+  // }
+  reloadStudents() {
+    this.students = [];
+    this.students.push({id: 'S001', name: 'Manoj'});
+    this.students.push({id: 'S002', name: 'Gayal'});
+    this.students.push({id: 'S003', name: 'Pethum'});
+    this.students.push({id: 'S005', name: 'Dinusha'});
+    this.students.push({id: 'S008', name: 'Chandimal'});
+  }
 }
